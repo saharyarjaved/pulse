3ba99c073ace99bd745b47e29a29f83c41233526
rakib <rakibjaved1826@gmail.com>
2025-12-06 00:05:43 +0530

    updated next.config.mjs and project fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "img.clerk.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+    ],
+  },
+
+  // Fix Server Actions error with tunnels
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "localhost:3000", // local
+        "127.0.0.1:3000", // local
+        // Add your devtunnel domain here (change this to your actual tunnel)
+        "fvg3sbs5-3000.inc1.devtunnels.ms",
+      ],
+    },
   },
 };
 
